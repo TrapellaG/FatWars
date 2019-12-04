dd131f0a025cfd1e3396df7d098bce1ce8bf99a4
TrapellaG <gerardcampos95@gmail.com>
2019-12-04 14:43:07 +0100

    2

--- FatWars/Assets/Scripts/Shoot.cs
+++ FatWars/Assets/Scripts/Shoot.cs
@@ -13,16 +13,29 @@ public class Shoot : MonoBehaviour
     {
 
     }
-
+    private void OnDrawGizmos()
+    {
+     
+    }
     // Update is called once per frame
     void Update()
     {
-        mousePos = Input.mousePosition;
-
+        
         if (Input.GetButtonDown("Fire1"))
         {
+            if (Input.GetMouseButtonDown(0)) //check if the LMB is clicked
+            {
+                RaycastHit hit;
+                Ray ray = GetComponentInChildren<Camera>().ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit)) //check if the ray hit something
+                {
+                    mousePos = hit.point; //use this position for what you want to do
+                    Debug.DrawRay(hit.point, -ray.direction);
+                }
+            }
             Rigidbody instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
-            instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed));
+            instantiatedProjectile.velocity = (mousePos - transform.position).normalized;//transform.TransformDirection(new Vector3(0, 0, speed));
         }
        
     }
